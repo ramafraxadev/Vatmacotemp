33b0c5e425c63fee462148afbdfa13f6acdbe591
ramafraxadev <ramafraxaxav@gmail.com>
2025-11-03 12:37:02 +0300

    Update export.vue

--- export.vue
+++ export.vue
@@ -1,103 +1,91 @@
 <template>
-    <div v-if="facturationExports_pageCharge">
-        <document-dasboard-export-list>
-          
-        </document-dasboard-export-list>
-        <documents-tableau-export
-        @documents-tableau-export-download-emise ="facturationExport_telecharger"
-        ></documents-tableau-export>
-    </div>
-</template>
+    <section class="documents_dashboard_export_list">
+        <mc-card
+            card-class="documents_dashboard_export_list_card"
+            card-width="max-content"
+            card-max-width="700px"
+        >
+            <div class="documents_dashboard_export_list_card_titre" test-id="test_ddch">
+                    <mc-title-heading heading-type="heading_3" 
+                    :heading-text="`${documentDasboardExportListTotal} exports`"
+                    ></mc-title-heading>
+                                
+            </div>
+            <div>
+                 <p class="documents_dashboard_export_list_card_titre_description" >
+                        Les exports de documents concernent des téléchargements
+                        <mc-link 
+                            link-color="pink" 
+                            link-text="des téléchargements et envois de plus de 10 documents." 
+                            link-href="https://help.macompta.fr/fr/articles/136042-modeles-de-documents-a-telecharger"
+                        ></mc-link>
+                        Le traitement d'un export prend généralement 1 à 2 minutes, mais peut potentiellement durer d'avantage.
+                        <mc-link 
+                            link-color="pink" 
+                            link-text="Une fois l'export traité." 
+                            link-href="https://help.macompta.fr/fr/articles/124677-600-generation-de-l-ecriture-de-paie"
+                        ></mc-link>
+                        celui-ci 
+                        <mc-link 
+                            link-class="{text-decoration: none;}"
+                            link-color="pink" 
+                            link-text="est disponible pendant trois jours sous forme d'une archive Zip." 
+                            link-href="https://help.macompta.fr/fr/articles/124845-conservation-des-donnees"
+                        ></mc-link>
+                    </p>  
+            </div>
 
-<script setup>
-    /***** Les imports vue *****/
-    import {  inject, onMounted,  ref } from 'vue'
-    /***** Les imports composant *****/
-    import DocumentDasboardExportList from './commun/DocumentDasboardExportList.vue'
-    import DocumentsTableauExport from './commun/DocumentsTableauExport.vue'
-    /***** Les imports store *****/
-    import { useFacturationDocumentsStore } from '../../stores/facturation/store-facturation-documents'
-    import { useLoaderStore } from '../../stores/commun/store-loader'
-
-    /**************************/
-    /**** Les injections ******/
-    /**************************/
-    const $mcNotify = inject('McNotify')
-
-    /**************************/
-    /**** Les stores **********/
-    /**************************/
-    const storeLoader = useLoaderStore()
-    const storeFD = useFacturationDocumentsStore()
-
-    const facturationExport_telecharger = async (index) => {
-        try {
-            await storeFD.action__facturationDocuments_telechargerArchive(index?.archive)
-        } catch (error) {
-            $mcNotify.error(error.message)
-        }
-    }
-
-     /**
-     * Recuperer la liste des exports de documents
-     * 
-     * @param {Boolean} estLoaderAffiche Si on affiche le loader ou non
-     */
-    const facturationExports_recupererDocumentsExports = async (estLoaderAffiche = true) => {
-        try {
-            if (estLoaderAffiche) {
-                storeLoader.action_launchLoader()
-            }
-
-            await storeFD.action__facturationDocuments_recupererListeExport(true)
-            
-            if (estLoaderAffiche) {
-                storeLoader.action_stopLoader()
-            }
-
-        } catch (error) {
-            if (estLoaderAffiche) {
-                storeLoader.action_stopLoader()
-            }
-            $mcNotify.error(error.message)
-        }
-    }
-
-
-   
-
-    /***************************************/
-    /**** OPERATIONS DE DOCUMENTS **********/
-    /***************************************/
-    const facturationExports_pageCharge = ref(false)
-
+            <div test-id="test_ddel">
+                <slot></slot>
+            </div>
+        </mc-card>
+    </section>
     
-    /**
-     * Charger tous les elements qui sont necessaires pour le composant
-     */
-     const facturationExports_preparer = async () => {
-        try {
-            storeLoader.action_launchLoader()
-            await facturationExports_recupererDocumentsExports(false)
-            storeLoader.action_stopLoader()
+</template>
+<script setup>
+    defineProps({
+        documentDasboardExportListTotal: {
+            type: Number,
+            required: true
+        }   
+    })
 
-            
-        } catch (error) {
-            storeLoader.action_stopLoader()
-            // certains appels d'API peuvent retourner une erreur sans message (ex: recuperer comptes generaux)
-            if (error.message) {
-                $mcNotify.error(error.message)
-            } else {
-                $mcNotify.error('Une erreur est survenue lors du chargement des données. Veuillez réessayer.')
-            }
-        }
-    }
-
-    /***************************************/
-    /**** CYCLE DE VIE DU COMPOSANT ********/
-    /***************************************/
-    onMounted(async () => {
-        await facturationExports_preparer()
-        facturationExports_pageCharge.value = true
-    }) 
 </script>
+
+<style lang="scss" scoped>
+    .documents_dashboard_export_list {
+        ::v-deep(.documents_dashboard_export_list_card) {
+            border-radius: 9999px;
+            background-color: #f8f8fc;
+            border: 1px solid #d1d1d1;
+            display: flex;
+            flex-direction: column;   
+            justify-content: center;  
+            text-align: center;  
+        }
+        @include mobile {
+            @include centerWithFlex(rowHorizontal, $gap: rem(40));
+            flex-wrap: wrap;
+            margin-bottom: rem(40);
+        }
+        &_card {
+            &_titre {
+                @include mobile {
+                    @include allFlexCenter();
+                    gap: rem(5);
+                    border-bottom: solid rem(1) $greyMedium;
+                    margin-bottom: rem(10);
+                    padding-bottom: rem(5);
+                    text-align: center; 
+                }
+                &_description {
+                    display: block; 
+                    margin-bottom: rem(10);
+                    line-height: 1.5;
+                }
+            }
+            
+        }
+        
+    }
+</style>
